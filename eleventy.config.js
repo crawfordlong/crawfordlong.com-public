77def5f2a46ad26f7dd0494a1d17f3cda1a2b4d5
Crawford Long <cbl@crawfordlong.com>
2024-01-25 15:25:32 -0500

    add markdownit to support excerpt rendering (I think)

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,6 @@
 const { DateTime } = require("luxon");
 const markdownItAnchor = require("markdown-it-anchor");
+const markdownIt = require("markdown-it");
 
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
