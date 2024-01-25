5a21ef9e236c9052d7e21a94405ea4665ed1dcbe
Crawford Long <cbl@crawfordlong.com>
2024-01-25 14:44:46 -0500

    Site metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "crawfordlong.com",
+	url: "https://crawfordlong.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Words and photos.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Crawford Long",
+		email: "cbl@crawfordlong.com",
+		url: "https://crawfordlong.com/about-me/"
 	}
 }
